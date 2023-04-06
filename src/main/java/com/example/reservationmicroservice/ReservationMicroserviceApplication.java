b54d457767b0276a9c1a70cd590fe6f5b9bbb0fb
unknown <kalinic.ra71.2019@uns.ac.rs>
2023-04-06 12:01:06 +0200

    setup microservice

---
+++ src/main/java/com/example/reservationmicroservice/ReservationMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.reservationmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationMicroserviceApplication.class, args);
+	}
+
+}
