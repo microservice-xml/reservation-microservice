71ad021d66464bd0041ce49043d616ccd7a5ed08
unknown <kalinic.ra71.2019@uns.ac.rs>
2023-05-07 09:14:10 +0200

    added crud operation for accomodation

--- src/main/java/com/example/reservationmicroservice/contoller/AccommodationController.java
+++ src/main/java/com/example/reservationmicroservice/contoller/AccommodationController.java
@@ -1,11 +1,63 @@
 package com.example.reservationmicroservice.contoller;
 
+import com.example.reservationmicroservice.model.Accommodation;
 import com.example.reservationmicroservice.service.AccommodationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/accommodation")
 public class AccommodationController {
     private final AccommodationService accommodationService;
+
+    @PostMapping
+    public ResponseEntity create(@RequestBody Accommodation accommodation){
+        try{
+            accommodationService.create(accommodation);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Successfully created.");
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Try again later...");
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity findAll(){
+        try{
+            return ResponseEntity.status(HttpStatus.OK).body(accommodationService.findAll());
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Try again later...");
+        }
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity findById(@PathVariable String id){
+        try{
+            return ResponseEntity.status(HttpStatus.OK).body(accommodationService.findById(id));
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Try again later...");
+        }
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity deleteById(@PathVariable String id){
+        try{
+            accommodationService.deleteById(id);
+            return ResponseEntity.status(HttpStatus.OK).body("Successfully deleted.");
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Try again later...");
+        }
+    }
+
+    @PutMapping
+    public ResponseEntity update(@RequestBody Accommodation accommodation){
+        try{
+            accommodationService.update(accommodation);
+            return ResponseEntity.status(HttpStatus.OK).body("Successfully updated.");
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Try again later...");
+        }
+    }
 }
--- src/main/java/com/example/reservationmicroservice/service/AccommodationService.java
+++ src/main/java/com/example/reservationmicroservice/service/AccommodationService.java
@@ -1,11 +1,33 @@
 package com.example.reservationmicroservice.service;
 
+import com.example.reservationmicroservice.model.Accommodation;
 import com.example.reservationmicroservice.repository.AccommodationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class AccommodationService {
     private final AccommodationRepository accommodationRepository;
+
+    public void create(Accommodation accommodation) {
+        accommodationRepository.save(accommodation);
+    }
+    public List<Accommodation> findAll() {
+        return accommodationRepository.findAll();
+    }
+    public Accommodation findById(String id){
+        return accommodationRepository.findById(id).get();
+    }
+    public void deleteById(String id){
+        accommodationRepository.deleteById(id);
+    }
+    public void update(Accommodation accommodation){
+        Accommodation acc = findById(accommodation.getId());
+        acc.setCity(accommodation.getCity());
+        acc.setAccommodationId(accommodation.getAccommodationId());
+        accommodationRepository.save(acc);
+    }
 }
