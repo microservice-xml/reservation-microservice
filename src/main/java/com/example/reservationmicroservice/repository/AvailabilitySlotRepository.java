db37a2a7b6ad80188daaca33fcc2874a6951c122
vukomanv <vladimir.vukoman.v@gmail.com>
2023-05-07 13:56:53 +0200

    Added find accommodation ids by ids and date range

--- src/main/java/com/example/reservationmicroservice/repository/AvailabilitySlotRepository.java
+++ src/main/java/com/example/reservationmicroservice/repository/AvailabilitySlotRepository.java
@@ -1,10 +1,17 @@
 package com.example.reservationmicroservice.repository;
 
+import com.example.reservationmicroservice.model.Accommodation;
 import com.example.reservationmicroservice.model.AvailabilitySlot;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface AvailabilitySlotRepository extends MongoRepository<AvailabilitySlot, String> {
+    @Query("{'accommodationId' : {'$in' : ?0}, 'start' :{ '$lte' : ?1, '$lte' :  ?2}, 'end' :{ '$gte' : ?1, '$gte' :  ?2}}")
+    List<AvailabilitySlot> findAllByIdAndAvailabilityRange(List<Long> appointmentIds, LocalDate startDate, LocalDate endDate);
 }
--- src/main/java/com/example/reservationmicroservice/service/AvailabilitySlotService.java
+++ src/main/java/com/example/reservationmicroservice/service/AvailabilitySlotService.java
@@ -1,10 +1,17 @@
 package com.example.reservationmicroservice.service;
 
 import com.example.reservationmicroservice.exception.AvailabilitySlotException;
+import com.example.reservationmicroservice.model.Accommodation;
 import com.example.reservationmicroservice.model.AvailabilitySlot;
+import com.example.reservationmicroservice.model.Reservation;
 import com.example.reservationmicroservice.repository.AvailabilitySlotRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -18,16 +25,16 @@ public class AvailabilitySlotService {
 
     private void isDateRangeValidForAdd(AvailabilitySlot availabilitySlot) {
         var availabilitySlots = availabilitySlotRepository.findAll();
-        for (AvailabilitySlot as: availabilitySlots) {
-            if (areDatesOverlapping(availabilitySlot, as)) {
+        for (AvailabilitySlot as : availabilitySlots) {
+            if (areDatesOverlapping(availabilitySlot.getStart(), availabilitySlot.getEnd(), as.getStart(), as.getEnd())) {
                 throw new AvailabilitySlotException("Date range of the slot you want to create overlaps with an existing one!");
             }
         }
     }
 
-    private boolean areDatesOverlapping(AvailabilitySlot availabilitySlot, AvailabilitySlot as) {
-        return !((availabilitySlot.getStart().isBefore(as.getStart()) && availabilitySlot.getEnd().isBefore(as.getStart()))
-                || (availabilitySlot.getStart().isAfter(as.getEnd()) && availabilitySlot.getEnd().isAfter(as.getEnd())));
+    private boolean areDatesOverlapping(LocalDate start, LocalDate end, LocalDate start2, LocalDate end2) {
+        return !((start.isBefore(start2) && end.isBefore(start2))
+                || (start.isAfter(end2) && end.isAfter(end2)));
     }
 
     public void edit(AvailabilitySlot availabilitySlot) {
@@ -44,10 +51,33 @@ public class AvailabilitySlotService {
 
     private void isDateRangeValidForEdit(AvailabilitySlot availabilitySlot) {
         var availabilitySlots = availabilitySlotRepository.findAll();
-        for (AvailabilitySlot as: availabilitySlots) {
-            if (areDatesOverlapping(availabilitySlot, as) && !as.getId().equals(availabilitySlot.getId())) {
+        for (AvailabilitySlot as : availabilitySlots) {
+            if (areDatesOverlapping(availabilitySlot.getStart(), availabilitySlot.getEnd(), as.getStart(), as.getEnd()) && !as.getId().equals(availabilitySlot.getId())) {
                 throw new AvailabilitySlotException("Date range of the slot you want to create overlaps with an existing one!");
             }
         }
+    }
+
+    public List<Long> getByAvailabilityRange(List<Long> appointmentIds, LocalDate startDate, LocalDate endDate) {
+        var availabilitySlots = availabilitySlotRepository.findAllByIdAndAvailabilityRange(appointmentIds, startDate, endDate);
+        if (availabilitySlots.size() == 0) {
+            throw new AvailabilitySlotException("There are no availability slots found");
+        }
+        ArrayList<Long> validAccommodation = getValidAccommodation(startDate, endDate, availabilitySlots);
+        return validAccommodation.stream().distinct().toList();
+    }
+
+    private ArrayList<Long> getValidAccommodation(LocalDate startDate, LocalDate endDate, List<AvailabilitySlot> availabilitySlots) {
+        var validAccommodation = new ArrayList<Long>();
+        for (AvailabilitySlot as : availabilitySlots) {
+            var valid = true;
+            for (Reservation res : as.getReservations()) {
+                if (areDatesOverlapping(startDate, endDate, res.getStart(), res.getEnd())) {
+                    valid = false;
+                }
+            }
+            if (valid) validAccommodation.add(as.getAccommodationId());
+        }
+        return validAccommodation;
     }
 }
