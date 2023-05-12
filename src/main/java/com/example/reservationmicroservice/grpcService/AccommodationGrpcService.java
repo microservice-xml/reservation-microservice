7f45cb948325118ded37fb131797537c1da8d308
vukomanv <vladimir.vukoman.v@gmail.com>
2023-05-12 20:42:59 +0200

    Implemented search accommodation functionality

--- src/main/java/com/example/reservationmicroservice/grpcService/AccommodationGrpcService.java
+++ src/main/java/com/example/reservationmicroservice/grpcService/AccommodationGrpcService.java
@@ -1,15 +1,14 @@
 package com.example.reservationmicroservice.grpcService;
 
-import com.example.reservationmicroservice.service.AccommodationService;
 import com.example.reservationmicroservice.service.AvailabilitySlotService;
-import com.example.reservationmicroservice.service.ReservationService;
-import communication.*;
+import communication.AccommodationServiceGrpc;
+import communication.SearchRequest;
+import communication.SearchResponse;
 import io.grpc.stub.StreamObserver;
 import lombok.RequiredArgsConstructor;
 import net.devh.boot.grpc.server.service.GrpcService;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @GrpcService
 @RequiredArgsConstructor
@@ -22,6 +21,7 @@ public class AccommodationGrpcService extends AccommodationServiceGrpc.Accommoda
         var startDate = LocalDate.of(request.getStartYear(), request.getStartMonth(), request.getStartDay());
         var endDate = LocalDate.of(request.getEndYear(), request.getEndMonth(), request.getEndDay());
         var accommodationIds = availabilitySlotService.getByAvailabilityRange(request.getAccommodationIdsList(), startDate, endDate);
+
         var searchResponse = communication.SearchResponse.newBuilder().addAllAccommodationIds(accommodationIds).build();
 
         responseStreamObserver.onNext(searchResponse);
